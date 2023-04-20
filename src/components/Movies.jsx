1b0debd0e22bfb0e2aea281c7b7fddccea215939
Oleg I <alinai-play@mail.ru>
2023-04-20 13:06:57 +0300

    first commit

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,9 +4,11 @@ import { Movie } from "./Movie";
 function Movies(props) {
     const { movies } = props;
     return <div className="movies">
-            {movies.map((movie) => (
-                <Movie key={movie.imdbID} {...movie} />
-            ))}
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <h4>Not found</h4>
+            )}
         </div> 
 };
 
--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -7,12 +7,13 @@ import { Search } from "../components/search";
 class Main extends React.Component {
     state = {
         movies: [],
+        loading: true,
     };
 
     componentDidMount() {
         fetch('http://www.omdbapi.com/?apikey=ab258471&s=Venom')
             .then((response) => response.json())
-            .then((data) => this.setState({ movies: data.Search }));
+            .then((data) => this.setState({ movies: data.Search, loading:false }));
     }
 
     searchMovies = (str, type= "all") => {
@@ -22,13 +23,14 @@ class Main extends React.Component {
             }`
         )
         .then((response) => response.json())
-        .then((data) => this.setState({ movies: data.Search }));
+        .then((data) => this.setState({ movies: data.Search, loading:false }));
     }
 
     render() {
-        const { movies } = this.state;
+        const { movies, loading } = this.state;
         return <main className="container content">
         <Search searchMovies={this.searchMovies}/>
+        {loading ? <Preloader/> : <Movies movies={this.state.movies} />}
         {
             movies.length ? (
                 <Movies movies={this.state.movies} />
